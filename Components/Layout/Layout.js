511afb3b067a3575d009acc7274d0a23f09a197e
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-19 23:22:02 +0530

    bg changed

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -2,11 +2,10 @@ import Head from "next/head";
 import React from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
-import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.body}>
+    <div>
       <Head>
         <title>Shop | V</title>
       </Head>
