51143616a89f2b1398e1c8dbed95d25062e93784
venkatesh-sdev <venkatesh.dev.main@gmail.com>
2022-12-19 23:27:17 +0530

    footer

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import React from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
